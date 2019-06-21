bc57f3baab6d40630cdf672102a87bd5162bb269
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-21 01:58:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   
   attr_accessor :name 
   
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
   
   
 end
