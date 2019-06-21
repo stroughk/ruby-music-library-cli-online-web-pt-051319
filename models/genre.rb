61449ebe18bc6d397e327534279cb1c639f97112
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-21 01:47:10 +0000

    Automatically backed up by Learn

--- models/genre.rb
+++ models/genre.rb
@@ -2,6 +2,25 @@ class Genre
   
   attr_accessor :name, :songs 
   
+    
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  
+  def initialize
+    save
+  end
+  
+  def save 
+    @@all << self 
+  end
+  
+  def self.reset_all
+    @@all.clear 
+  end 
   
   
   
