7eedb499a5f0c513edeeb7a066b60a28f0d80c97
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-21 01:59:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,15 @@ class Song
   
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name, artist = nil, genre = nil)
     @name = name 
     self.artist = artist if artist
     self.genre = genre if genre
   end
+  
+  
   
   
 end
